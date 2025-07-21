5070328236e20269325ea51ac8a4c460301d7dc2
Arpit-Nakrani-Networked <arpit.nakrani@networked.co>
2025-07-21 14:45:08 +0200

    fix(base url change for redirect)

--- src/generic/PermissionDenied.jsx
+++ src/generic/PermissionDenied.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from '@edx/frontend-platform/i18n';
-import { Alert, Button, Icon } from '@openedx/paragon';
+import { Alert, Button } from '@openedx/paragon';
+import { NETWORKED_FRONTEND_URL } from 'helper/constants';
 // import { LockFill } from '@openedx/paragon/icons';
 
 const PermissionDenied = ({ courseId }) => {
   const handleBackToCourse = () => {
-    window.location.href = `http://localhost:3000/courses/${courseId}`;
+    window.location.href = `${NETWORKED_FRONTEND_URL}/courses/${courseId}`;
   };
 
   return (
