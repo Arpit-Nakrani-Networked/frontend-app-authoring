ad796273d5c11bfcefc46075a7a5a29fff3eb0af
Arpit-Nakrani-Networked <arpit.nakrani@networked.co>
2025-10-10 09:56:16 +0200

    FIX | base url change of networked

--- src/helper/constants.ts
+++ src/helper/constants.ts
@@ -1,2 +1,27 @@
-export const NETWORKED_FRONTEND_URL = 'https://app.lab.networked.co';
-export const NETWORKED_BACKEND_URL = 'https://backend.lab.networked.co';
+let env: 'lab' | 'qa' | 'prod' = 'qa'; // default to QA
+
+if (typeof window !== 'undefined') {
+  const host = window.location.hostname; // e.g. apps.courses.lab.networked.co
+
+  if (host.includes('lab')) {
+    env = 'lab';
+  } else if (host.includes('qa')) {
+    env = 'qa';
+  } else {
+    env = 'prod';
+  }
+}
+
+export const NETWORKED_FRONTEND_URL =
+  env === 'lab'
+    ? 'https://app.lab.networked.co'
+    : env === 'qa'
+    ? 'https://app.qa.networked.co'
+    : 'https://app.networked.co';
+
+export const NETWORKED_BACKEND_URL =
+  env === 'lab'
+    ? 'https://backend.lab.networked.co'
+    : env === 'qa'
+    ? 'https://backend.qa.networked.co'
+    : 'https://backend.networked.co';
